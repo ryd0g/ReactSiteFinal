6def42ab1087a3af955aadc0d6eefe4711addaa4
Ryan Lee <rjlguitar@gmail.com>
2020-12-08 23:15:23 -0800

    added imports, page structure and theme

--- ship-x/src/App.js
+++ ship-x/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Nav';
+import Title from './Title';
+import Features from './Features';
+import Form from './Form';
+import Footer from './Footer';
+import 'bootswatch/dist/lux/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar />
+      <Title />
+      <Features />
+      <Form />
+      <Footer />
     </div>
   );
 }
