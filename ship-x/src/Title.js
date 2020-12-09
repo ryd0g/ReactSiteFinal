caffeafdc0676791a788056b22e7c1d86458b7c5
Ryan Lee <rjlguitar@gmail.com>
2020-12-09 10:25:07 -0800

    fixed title button

--- ship-x/src/Title.js
+++ ship-x/src/Title.js
@@ -7,6 +7,9 @@ function Title() {
       <div class='jumbotron text-center'>
         <h1>Ship-X</h1>
         <p class='lead'>Getting your products shipped the right way!</p>
+        <a href='about.html' class='btn btn-primary'>
+          Learn More
+        </a>
       </div>
     </section>
   );
