10f33cd961080833ad48a7fe83d62f58367ffdfb
Ryan Lee <rjlguitar@gmail.com>
2020-12-08 23:16:06 -0800

    made navbar component

---
+++ ship-x/src/Nav.js
@@ -0,0 +1,32 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Nav.css';
+
+function Navbar() {
+  return (
+    <nav class='navbar sticky-top navbar-expand navbar-dark bg-primary'>
+      <a class='navbar-brand' href='homepage.html'>
+        Ship-X
+      </a>
+      <div class='navbar-nav'>
+        <div class='dropdown'>
+          <a
+            class='nav-link dropdown-toggle'
+            data-toggle='dropdown'
+            href='#'
+            role='button'
+            aria-haspopup='true'
+            aria-expanded='true'>
+            Menu
+          </a>
+          <div class='dropdown-menu' aria-labelledby='dropdownMenuButton'>
+            <a class='dropdown-item' href='homepage.html'>
+              Home
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
