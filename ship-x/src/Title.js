c090b8ce284eab0e2fbb3e99185ddbbabc0d4b00
Ryan Lee <rjlguitar@gmail.com>
2020-12-08 23:16:25 -0800

    made title component

---
+++ ship-x/src/Title.js
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Title.css';
+
+function Title() {
+  return (
+    <section id='hero'>
+      <div class='jumbotron text-center'>
+        <h1>Ship-X</h1>
+        <p class='lead'>Getting your products shipped the right way!</p>
+        <a href='about.html' class='btn btn-primary'>
+          Learn More
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Title;
